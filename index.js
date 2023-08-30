0d2346cce8f068c69a435b2c800c6fad04479dee
Sagar Soneja_ACEIT <sagarsoneja89@gmail.com>
2023-08-30 13:15:32 +0530

    fifth commit

--- index.js
+++ index.js
@@ -30,46 +30,65 @@ const u = document.getElementById("callForm")
 //     console.log("mouseout => ",event.target);
 // });
 
-const title = document.getElementById('main-header')
-title.style.border = 'solid 10px black'
+// const title = document.getElementById('main-header')
+// title.style.border = 'solid 10px black'
 
 
-const addItem = document.querySelector('.title')
-addItem.style.fontWeight = 'bold'
-addItem.style.color = 'green'
+// const addItem = document.querySelector('.title')
+// addItem.style.fontWeight = 'bold'
+// addItem.style.color = 'green'
 
-const listelement = document.querySelector('.list-group-item:nth-child(2)')
-listelement.style.background = 'green'
+// const listelement = document.querySelector('.list-group-item:nth-child(2)')
+// listelement.style.background = 'green'
 
-const list = document.querySelectorAll('.list-group-item')
+// const list = document.querySelectorAll('.list-group-item')
 
-list.forEach(l => {
-    l.style.fontWeight = 'bold'
-})
-list.style.fontWeight = 'bold'
-
-
+// list.forEach(l => {
+//     l.style.fontWeight = 'bold'
+// })
+// list.style.fontWeight = 'bold'
 
 
-const newLi = document.createElement("li");
-newLi.textContent = "New Item";
-newLi.className = "list-group-item new-item"; 
 
 
-const ul = document.querySelector(".items");
-ul.appendChild(newLi);
+// const newLi = document.createElement("li");
+// newLi.textContent = "New Item";
+// newLi.className = "list-group-item new-item"; 
 
 
-const itemsWithClass = document.getElementsByClassName("new-item");
-if (itemsWithClass.length > 0) {
-    itemsWithClass[0].textContent = "Edited with getElementsByClassName";
-}
+// const ul = document.querySelector(".items");
+// ul.appendChild(newLi);
 
 
-const listItems = document.getElementsByTagName("li");
-for (const item of listItems) {
-    if (item.classList.contains("new-item")) {
-        item.textContent = "Edited with getElementsByTagName";
-        break; 
-    }
-}
+// const itemsWithClass = document.getElementsByClassName("new-item");
+// if (itemsWithClass.length > 0) {
+//     itemsWithClass[0].textContent = "Edited with getElementsByClassName";
+// }
+
+
+// const listItems = document.getElementsByTagName("li");
+// for (const item of listItems) {
+//     if (item.classList.contains("new-item")) {
+//         item.textContent = "Edited with getElementsByTagName";
+//         break; 
+//     }
+// }
+
+
+//-------------------- fift commit---------------------------------------
+
+
+const secondItem = document.querySelector("#items li:nth-child(2)");
+secondItem.style.backgroundColor = "green";
+
+// Make the 3rd item invisible
+const thirdItem = document.querySelector("#items li:nth-child(3)");
+thirdItem.style.display = "none";
+
+
+
+
+const oddItems = document.querySelectorAll("#items li:nth-child(odd)");
+oddItems.forEach(item => {
+    item.style.backgroundColor = "green";
+});
