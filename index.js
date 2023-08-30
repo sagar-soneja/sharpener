786325b59d3be13932ac61da454697085db4f95a
Sagar Soneja_ACEIT <sagarsoneja89@gmail.com>
2023-08-30 12:46:45 +0530

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const u = document.getElementById("callForm")
-u.addEventListener("submit", function(event) {
-    event.preventDefault();
+// u.addEventListener("submit", function(event) {
+//     event.preventDefault();
     
-    const formData = new FormData(event.target);
-    const formObject = {};
+//     const formData = new FormData(event.target);
+//     const formObject = {};
     
-    formData.forEach((value, key) => {
-        formObject[key] = value;
-    });
+//     formData.forEach((value, key) => {
+//         formObject[key] = value;
+//     });
     
-    console.log("submit => ",formObject);
-});
+//     console.log("submit => ",formObject);
+// });
 
 
-u.addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("click => ",event.target);
-});
+// u.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     console.log("click => ",event.target);
+// });
 
-u.addEventListener("mouseover", function(event) {
-    event.preventDefault();
-    console.log("mourseover => ",event.target);
-});
+// u.addEventListener("mouseover", function(event) {
+//     event.preventDefault();
+//     console.log("mourseover => ",event.target);
+// });
 
-u.addEventListener("mouseout", function(event) {
-    event.preventDefault();
+// u.addEventListener("mouseout", function(event) {
+//     event.preventDefault();
     
     
-    console.log("mouseout => ",event.target);
-});
+//     console.log("mouseout => ",event.target);
+// });
+
+const title = document.getElementById('main-header')
+title.style.border = 'solid 10px black'
+
+
+const addItem = document.querySelector('.title')
+addItem.style.fontWeight = 'bold'
+addItem.style.color = 'green'
